f89be8fe93089e3caa935e1778a9e79b59f529c7
Andrew D. Yates <andrew.yates@hhmds.com>
2010-08-12 21:07:25 -0400

    minor style updates

--- __init__.py
+++ __init__.py
@@ -4,6 +4,10 @@
 # All Rights Reserved.
 """Integrate PDF miner terminal application into App Engine.
 
+This could be extended to use all of pdfminer's functionality
+including XML conversion, table of contents extraction, and embedded
+jpeg image extraction.
+
 PDFMiner by Yusuke Shinyama
 http://www.unixuser.org/~euske/python/pdfminer
 """
@@ -12,11 +16,10 @@ __authors__ = ['"Andrew D. Yates", <andrew.yates@hhmds.com>']
 
 import StringIO
 
-from pdfminer.pdfparser import PDFParser, PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfdevice import PDFDevice
 from pdfminer import converter
-from pdfminer.layout import LAParams
+from pdfminer import layout
+from pdfminer import pdfinterp
+from pdfminer import pdfparser
 
 
 def pdf_to_text(pdf):
@@ -30,22 +33,25 @@ def pdf_to_text(pdf):
   Returns:
     str of text extracted from `pdf` contents.
   """
-  DEVICE = converter.TextConverter
-  IN_BUFFER = StringIO.StringIO(pdf)
-  OUT_BUFFER = StringIO.StringIO()
-  
-  parser = PDFParser(IN_BUFFER)
-  doc = PDFDocument()
+  # make input and output buffers
+  in_buffer = StringIO.StringIO(pdf)
+  out_buffer = StringIO.StringIO()
+
+  # configure pdf parser
+  parser = pdfparser.PDFParser(in_buffer)
+  doc = pdfparser.PDFDocument()
   parser.set_document(doc)
   doc.set_parser(parser)
   doc.initialize(password='')
-  rsrcmgr = PDFResourceManager()
-  laparams = LAParams()
-  device = DEVICE(rsrcmgr, outfp=OUT_BUFFER, codec='utf-8', laparams=laparams)
-  interpreter = PDFPageInterpreter(rsrcmgr, device)
+  rsrcmgr = pdfinterp.PDFResourceManager()
+  laparams = layout.LAParams()
+  
+  # convert pdf to text
+  device = converter.TextConverter(
+    rsrcmgr, outfp=out_buffer, codec='utf-8', laparams=laparams)
+  interpreter = pdfinterp.PDFPageInterpreter(rsrcmgr, device)
 
   for page in doc.get_pages():
     interpreter.process_page(page)
 
-  return OUT_BUFFER.getvalue()
-
+  return out_buffer.getvalue()
